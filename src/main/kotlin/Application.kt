41431bb4836aeec17acfa29737c1936a2e8c9b67
Naruedon-mu <marknarudon@gmail.com>
2025-06-29 14:08:36 +0700

    Initial commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package com.naruedon672110147
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
