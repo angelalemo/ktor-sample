ea63b87ed46f2ce6ddddbff688312c86e2093c48
Naruedon-mu <marknarudon@gmail.com>
2025-07-05 13:28:08 +0700

    Workshop #4

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "2.1.10"
     id("io.ktor.plugin") version "3.2.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23" // Must match Kotlin version
 }
 
 group = "com.naruedon672110147"
@@ -23,6 +24,12 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-config-yaml")
+
+    // Content Negotiation & JSON Serialization
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+
 }
