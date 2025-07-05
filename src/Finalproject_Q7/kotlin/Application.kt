8de5d98b6f6f467606e5ae13a558556ba906b2ce
Naruedon-mu <marknarudon@gmail.com>
2025-07-05 14:07:51 +0700

    .

---
+++ src/Finalproject_Q7/kotlin/Application.kt
@@ -0,0 +1,17 @@
+package com.naruedon672110147
+
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+    configureRouting()
+}
+
